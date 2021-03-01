b5271350e8deb672edb9932e4701cdcd00242b00
Sunshine <sunshine@uberspace.net>
2021-02-28 16:28:28 -1000

    update for WoW 9.0.2

---
+++ ShadowMeld/ShadowMeld.lua
@@ -0,0 +1,19 @@
+﻿function ShadowMeld_OnLoad(self)
+	self:RegisterEvent("PLAYER_ENTERING_WORLD");
+	self:RegisterEvent("UPDATE_STEALTH");
+end
+
+function ShadowMeld_OnEvent(self, event, ...)
+	if ( event == "PLAYER_ENTERING_WORLD" ) then
+		if ( IsStealthed() ) then
+			self:Show();
+		end
+	end
+	if ( event == "UPDATE_STEALTH" ) then
+		if ( IsStealthed() ) then
+			UIFrameFadeIn(self, 0.15, 0, 1);
+		else
+			UIFrameFadeOut(self, 0.1, 1, 0);
+		end
+	end
+end
