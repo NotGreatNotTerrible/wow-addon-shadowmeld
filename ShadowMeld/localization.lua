7d14db33e7e5f813ce134f629098958e4a6ce9dc
Sunshine <sunshine@uberspace.net>
2021-02-28 16:06:13 -1000

    add code from v2.1

---
+++ ShadowMeld/localization.lua
@@ -0,0 +1,21 @@
+﻿if (GetLocale() == "deDE") then
+	SM_STEALTH = "Verstohlenheit";
+	SM_PROWL = "Schleichen";
+	SM_SHADOWMELD = "Schattenmimik";
+elseif (GetLocale() == "frFR") then
+	SM_STEALTH = "Camouflage";
+	SM_PROWL = "Rôder";
+	SM_SHADOWMELD = "Camouflage dans l'ombre";
+elseif (GetLocale() == "esES") then
+	SM_STEALTH = "Sigilo";
+	SM_PROWL = "Acechar";
+	SM_SHADOWMELD = "Fusión de las Sombras";
+elseif (GetLocale() == "ruRU") then
+	SM_STEALTH = "Незаметность";
+	SM_PROWL = "Крадущийся зверь";
+	SM_SHADOWMELD = "Слиться с тенью";
+else
+	SM_STEALTH = "Stealth";
+	SM_PROWL = "Prowl";
+	SM_SHADOWMELD = "Shadowmeld";
+end
